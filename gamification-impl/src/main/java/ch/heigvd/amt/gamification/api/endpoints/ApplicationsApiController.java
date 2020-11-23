5bf66a3f9a740c97dc5dc70bc03f2f0675e4a99c
Naludrag <stephanecarvalho81@gmail.com>
2020-11-23 14:08:23 +0100

    Merged with dev

--- gamification-impl/src/main/java/ch/heigvd/amt/gamification/api/endpoints/ApplicationsApiController.java
+++ gamification-impl/src/main/java/ch/heigvd/amt/gamification/api/endpoints/ApplicationsApiController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.servlet.ServletRequest;
 import javax.validation.Valid;
 
 @Controller
