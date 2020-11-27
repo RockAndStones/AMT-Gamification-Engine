07845caa6f400ce5c0a8c1f08ea356d65b154b1a
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-11-27 15:30:23 +0100

    Implemented /applications/{name} api

--- gamification-impl/src/main/java/ch/heigvd/amt/gamification/api/endpoints/ApplicationsApiController.java
+++ gamification-impl/src/main/java/ch/heigvd/amt/gamification/api/endpoints/ApplicationsApiController.java
@@ -10,10 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @Controller
 public class ApplicationsApiController implements ApplicationsApi {
@@ -28,9 +31,23 @@ public class ApplicationsApiController implements ApplicationsApi {
         appEntity.setName(inlineObject.getName());
         applicationRepository.save(appEntity);
 
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{name}")
+                .buildAndExpand(appEntity.getName()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @Override
+    public ResponseEntity<Application> getApplication(@ApiParam(required = true) @PathVariable("name") String name) {
+        ApplicationEntity appEntity = applicationRepository.findByName(name);
+        if (appEntity == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
         Application app = new Application();
         app.setName(appEntity.getName());
         app.setApiKey(appEntity.getApiKey());
+
         return ResponseEntity.ok(app);
     }
 }
