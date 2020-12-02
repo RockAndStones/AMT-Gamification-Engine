972bd3733b5d6746173d4ec0832785f1f6d105eb
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-12-02 22:25:47 +0100

    Created new function to sum points

--- gamification-impl/src/main/java/ch/heigvd/amt/gamification/repositories/PointsUserRepository.java
+++ gamification-impl/src/main/java/ch/heigvd/amt/gamification/repositories/PointsUserRepository.java
@@ -1,8 +1,14 @@
 package ch.heigvd.amt.gamification.repositories;
 
 import ch.heigvd.amt.gamification.entities.PointsUserEntity;
+import ch.heigvd.amt.gamification.entities.UserEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface PointsUserRepository extends CrudRepository<PointsUserEntity, Long> {
     PointsUserEntity findPointsByUserIdAndPointScaleId(long userId, long pointScaleId);
+
+    @Query("SELECT SUM(points) FROM PointsUserEntity WHERE user=:user")
+    double sumPointsByUser(@Param("user") UserEntity u);
 }
