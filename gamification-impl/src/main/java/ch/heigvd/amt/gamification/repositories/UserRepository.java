df4c3d24c01af489868cb06361534d9f2e7195e2
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-12-02 23:00:17 +0100

    Added user rankings function to user repository

--- gamification-impl/src/main/java/ch/heigvd/amt/gamification/repositories/UserRepository.java
+++ gamification-impl/src/main/java/ch/heigvd/amt/gamification/repositories/UserRepository.java
@@ -1,9 +1,27 @@
 package ch.heigvd.amt.gamification.repositories;
 
+import ch.heigvd.amt.gamification.api.model.Application;
 import ch.heigvd.amt.gamification.entities.UserEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+interface UserRanking {
+    String getUserId();
+    int getPoints();
+}
 
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
     Iterable<UserEntity> findAllByAppApiKey(String apiKey);
     UserEntity findByUserAppIdAndAppApiKey(String userAppId, String apiKey);
+
+    @Query(value =
+            "SELECT " +
+                "ue.userAppId AS userId, " +
+                "SUM(pue.points) AS points " +
+            "FROM UserEntity AS ue " +
+                "JOIN PointsUserEntity AS pue " +
+            "WHERE ue.app = :app " +
+            "GROUP BY ue.userAppId")
+    Iterable<UserRanking> getUserRankingsOfApp(@Param("app") Application app);
 }
