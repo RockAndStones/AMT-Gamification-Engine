f966964646c8631508f20b6e27386853960a7502
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-12-10 13:30:58 +0100

    Updated JPQL queries

--- gamification-impl/src/main/java/ch/heigvd/amt/gamification/repositories/PointsHistoryRepository.java
+++ gamification-impl/src/main/java/ch/heigvd/amt/gamification/repositories/PointsHistoryRepository.java
@@ -9,14 +9,14 @@ import java.util.List;
 
 public interface PointsHistoryRepository extends CrudRepository<PointsHistoryEntity, Long> {
     @Query(value =
-            "SELECT PointsHistoryEntity " +
-            "FROM PointsHistoryEntity " +
-            "WHERE event.userAppId = :user")
+            "SELECT phe " +
+            "FROM PointsHistoryEntity AS phe " +
+            "WHERE phe.event.userAppId = :user")
     List<PointsHistoryEntity> findAllByUser(String user);
 
     @Query(value =
-            "SELECT PointsHistoryEntity " +
-            "FROM PointsHistoryEntity " +
-            "WHERE event.userAppId = :user AND pointScale = :pointScale")
+            "SELECT phe " +
+            "FROM PointsHistoryEntity AS phe " +
+            "WHERE phe.event.userAppId = :user AND phe.pointScale = :pointScale")
     List<PointsHistoryEntity> findAllByUserAndPointScale(String user, PointScaleEntity pointScale);
 }
