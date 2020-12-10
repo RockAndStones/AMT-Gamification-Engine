0fa5cc86433e8340087b0c07958841a14ea1a30b
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-12-10 21:20:03 +0100

    Implemented user progression by pointscale

--- gamification-impl/src/main/java/ch/heigvd/amt/gamification/api/endpoints/StatisticsApiController.java
+++ gamification-impl/src/main/java/ch/heigvd/amt/gamification/api/endpoints/StatisticsApiController.java
@@ -4,8 +4,10 @@ import ch.heigvd.amt.gamification.api.StatisticsApi;
 import ch.heigvd.amt.gamification.api.model.PointsProgression;
 import ch.heigvd.amt.gamification.api.model.PointsProgressionData;
 import ch.heigvd.amt.gamification.entities.ApplicationEntity;
+import ch.heigvd.amt.gamification.entities.PointScaleEntity;
 import ch.heigvd.amt.gamification.entities.PointsHistoryEntity;
 import ch.heigvd.amt.gamification.entities.UserEntity;
+import ch.heigvd.amt.gamification.repositories.PointScaleRepository;
 import ch.heigvd.amt.gamification.repositories.PointsHistoryRepository;
 import ch.heigvd.amt.gamification.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,9 @@ import java.util.stream.Collectors;
 public class StatisticsApiController implements StatisticsApi {
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    PointScaleRepository pointScaleRepository;
 
     @Autowired
     PointsHistoryRepository pointsHistoryRepository;
@@ -36,6 +41,27 @@ public class StatisticsApiController implements StatisticsApi {
         PointsProgression pointsProgression = new PointsProgression();
         pointsProgression.setUserAppId(userAppId);
         pointsProgression.setData(pointsHistoryRepository.findAllByUser(userAppId)
+                .stream()
+                .map(this::pointsHistoryEntityToData)
+                .collect(Collectors.toList()));
+
+        return ResponseEntity.ok(pointsProgression);
+    }
+
+    @Override
+    public ResponseEntity<PointsProgression> getUserProgressionByPointScale(String userAppId, Long pointScaleId) {
+        ApplicationEntity app = (ApplicationEntity) request.getAttribute("ApplicationEntity");
+        UserEntity u = userRepository.findByUserAppIdAndAppApiKey(userAppId, app.getApiKey());
+        if (u == null)
+            return ResponseEntity.notFound().build();
+
+        PointScaleEntity p = pointScaleRepository.findByIdAndAppApiKey(pointScaleId, app.getApiKey());
+        if (p == null)
+            return ResponseEntity.notFound().build();
+
+        PointsProgression pointsProgression = new PointsProgression();
+        pointsProgression.setUserAppId(userAppId);
+        pointsProgression.setData(pointsHistoryRepository.findAllByUserAndPointScale(userAppId, p)
                 .stream()
                 .map(this::pointsHistoryEntityToData)
                 .collect(Collectors.toList()));
