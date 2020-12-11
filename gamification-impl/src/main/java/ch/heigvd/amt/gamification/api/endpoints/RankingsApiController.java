6b3a560205cdecdf26863cc58bfca24dc1046075
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-12-11 11:37:54 +0100

    Updated RankingsApi to use new data structures

--- gamification-impl/src/main/java/ch/heigvd/amt/gamification/api/endpoints/RankingsApiController.java
+++ gamification-impl/src/main/java/ch/heigvd/amt/gamification/api/endpoints/RankingsApiController.java
@@ -1,11 +1,7 @@
 package ch.heigvd.amt.gamification.api.endpoints;
 
 import ch.heigvd.amt.gamification.api.RankingsApi;
-import ch.heigvd.amt.gamification.api.model.PaginatedUserRanking;
-import ch.heigvd.amt.gamification.api.model.Pagination;
-import ch.heigvd.amt.gamification.api.model.PointScale;
-import ch.heigvd.amt.gamification.api.model.UserRanking;
-import ch.heigvd.amt.gamification.dto.UserRankingDTO;
+import ch.heigvd.amt.gamification.api.model.*;
 import ch.heigvd.amt.gamification.entities.ApplicationEntity;
 import ch.heigvd.amt.gamification.entities.PointScaleEntity;
 import ch.heigvd.amt.gamification.repositories.PointScaleRepository;
@@ -14,16 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Controller
 public class RankingsApiController implements RankingsApi {
@@ -37,18 +29,14 @@ public class RankingsApiController implements RankingsApi {
     HttpServletRequest request;
 
     @Override
-    public ResponseEntity<PaginatedUserRanking> getRankingsByTotalPoints(@Valid Integer page, @Valid Integer pageSize) {
+    public ResponseEntity<PaginatedPointsRankings> getRankingsByTotalPoints(@Valid Integer page, @Valid Integer pageSize) {
         ApplicationEntity app = (ApplicationEntity) request.getAttribute("ApplicationEntity");
 
-        // todo implement pagination
         Pageable pageable = PageRequest.of(page, pageSize);
-        Page<UserRankingDTO> userRankings = userRepository.userRankingsByTotalPoints(app, pageable);
+        Page<PointsRanking> userRankings = userRepository.userRankingsByTotalPoints(app, pageable);
 
-        PaginatedUserRanking p = new PaginatedUserRanking();
-        p.data(userRankings.stream()
-                .map(this::userRankingFromDTO)
-                .collect(Collectors.toList())
-        );
+        PaginatedPointsRankings p = new PaginatedPointsRankings();
+        p.data(userRankings.getContent());
 
         Pagination pagination = new Pagination();
         pagination.setNumberOfItems(userRankings.getTotalElements());
@@ -72,58 +60,38 @@ public class RankingsApiController implements RankingsApi {
     }
 
     @Override
-    public ResponseEntity<PaginatedUserRanking> getRankingsByEventTypePoints(String eventType) {
+    public ResponseEntity<PaginatedPointsRankings> getRankingsByEventTypePoints(String eventType) {
         ApplicationEntity app = (ApplicationEntity) request.getAttribute("ApplicationEntity");
 
-        List<UserRankingDTO> userRankings = userRepository.userRankingsByTotalPoints(app, eventType);
-        PaginatedUserRanking p = new PaginatedUserRanking();
-
-        p.data(userRankings.stream()
-                .map(this::userRankingFromDTO)
-                .collect(Collectors.toList())
-        );
+        List<PointsRanking> userRankings = userRepository.userRankingsByTotalPoints(app, eventType);
+        PaginatedPointsRankings p = new PaginatedPointsRankings();
+        p.data(userRankings);
 
         return ResponseEntity.ok(p);
     }
 
     @Override
-    public ResponseEntity<PaginatedUserRanking> getRankingsByPointScalesPoints(Long id) {
+    public ResponseEntity<PaginatedPointsRankings> getRankingsByPointScalesPoints(Long id) {
         ApplicationEntity app = (ApplicationEntity) request.getAttribute("ApplicationEntity");
         PointScaleEntity pointScale = pointScaleRepository.findByIdAndAppApiKey(id, app.getApiKey());
         if (pointScale == null)
             return ResponseEntity.notFound().build();
 
-        List<UserRankingDTO> userRankings = userRepository.userRankingsByTotalPoints(app, pointScale);
-        PaginatedUserRanking p = new PaginatedUserRanking();
-
-        p.data(userRankings.stream()
-                .map(this::userRankingFromDTO)
-                .collect(Collectors.toList())
-        );
+        List<PointsRanking> userRankings = userRepository.userRankingsByTotalPoints(app, pointScale);
+        PaginatedPointsRankings p = new PaginatedPointsRankings();
+        p.data(userRankings);
 
         return ResponseEntity.ok(p);
     }
 
     @Override
-    public ResponseEntity<PaginatedUserRanking> getRankingsByTotalBadges() {
+    public ResponseEntity<PaginatedBadgesRankings> getRankingsByTotalBadges() {
         ApplicationEntity app = (ApplicationEntity) request.getAttribute("ApplicationEntity");
 
-        List<UserRankingDTO> userRankings = userRepository.userRankingsByBadges(app);
-        PaginatedUserRanking p = new PaginatedUserRanking();
-
-        p.data(userRankings.stream()
-                .map(this::userRankingFromDTO)
-                .collect(Collectors.toList())
-        );
+        List<BadgesRanking> userRankings = userRepository.userRankingsByBadges(app);
+        PaginatedBadgesRankings p = new PaginatedBadgesRankings();
+        p.data(userRankings);
 
         return ResponseEntity.ok(p);
-    }
-
-    private UserRanking userRankingFromDTO(UserRankingDTO userRankingDTO) {
-        UserRanking ur = new UserRanking();
-        ur.setUserId(userRankingDTO.getUserId());
-        ur.setPoints(userRankingDTO.getPoints());
-
-        return ur;
     }
 }
--- gamification-impl/src/main/java/ch/heigvd/amt/gamification/repositories/UserRepository.java
+++ gamification-impl/src/main/java/ch/heigvd/amt/gamification/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package ch.heigvd.amt.gamification.repositories;
 
-import ch.heigvd.amt.gamification.dto.UserRankingDTO;
+import ch.heigvd.amt.gamification.api.model.BadgesRanking;
+import ch.heigvd.amt.gamification.api.model.PointsRanking;
 import ch.heigvd.amt.gamification.entities.ApplicationEntity;
 import ch.heigvd.amt.gamification.entities.PointScaleEntity;
 import ch.heigvd.amt.gamification.entities.UserEntity;
@@ -18,29 +19,29 @@ public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
     @Query(value =
             "SELECT " +
-                "new ch.heigvd.amt.gamification.dto.UserRankingDTO(ue.userAppId, COALESCE(SUM(pue.points), 0)) " +
+                "new ch.heigvd.amt.gamification.model.CustomPointsRanking(ue.userAppId, COALESCE(SUM(pue.points), 0.0)) " +
             "FROM UserEntity AS ue " +
             "LEFT JOIN PointsUserEntity AS pue " +
                 "ON ue = pue.user " +
             "WHERE ue.app = :app " +
             "GROUP BY ue.userAppId " +
             "ORDER BY COALESCE(SUM(pue.points), 0) DESC")
-    Page<UserRankingDTO> userRankingsByTotalPoints(@Param("app") ApplicationEntity app, Pageable p);
+    Page<PointsRanking> userRankingsByTotalPoints(@Param("app") ApplicationEntity app, Pageable p);
 
     @Query(value =
             "SELECT " +
-                "new ch.heigvd.amt.gamification.dto.UserRankingDTO(ue.userAppId, COALESCE(SUM(pue.points), 0)) " +
+                "new ch.heigvd.amt.gamification.model.CustomPointsRanking(ue.userAppId, COALESCE(SUM(pue.points), 0.0)) " +
             "FROM UserEntity AS ue " +
             "LEFT JOIN PointsUserEntity AS pue " +
                 "ON ue = pue.user " +
             "WHERE ue.app = :app AND pue.pointScale = :pointScale " +
             "GROUP BY ue.userAppId " +
             "ORDER BY COALESCE(SUM(pue.points), 0) DESC")
-    List<UserRankingDTO> userRankingsByTotalPoints(@Param("app") ApplicationEntity app, @Param("pointScale") PointScaleEntity pointScale);
+    List<PointsRanking> userRankingsByTotalPoints(@Param("app") ApplicationEntity app, @Param("pointScale") PointScaleEntity pointScale);
 
     @Query(value =
             "SELECT " +
-                "new ch.heigvd.amt.gamification.dto.UserRankingDTO(ue.userAppId, COALESCE(SUM(pue.points), 0)) " +
+                "new ch.heigvd.amt.gamification.model.CustomPointsRanking(ue.userAppId, COALESCE(SUM(pue.points), 0.0)) " +
             "FROM UserEntity AS ue " +
             "LEFT JOIN PointsUserEntity AS pue ON ue = pue.user " +
             "LEFT JOIN PointScaleEntity AS pse ON pue.pointScale = pse " +
@@ -48,13 +49,13 @@ public interface UserRepository extends CrudRepository<UserEntity, Long> {
             "WHERE re.app = :app AND re.eventType = :eventType " +
             "GROUP BY ue.id, ue.userAppId " +
             "ORDER BY COALESCE(SUM(pue.points), 0) DESC")
-    List<UserRankingDTO> userRankingsByTotalPoints(@Param("app") ApplicationEntity app, @Param("eventType") String eventType);
+    List<PointsRanking> userRankingsByTotalPoints(@Param("app") ApplicationEntity app, @Param("eventType") String eventType);
 
     @Query(value =
             "SELECT " +
-                "new ch.heigvd.amt.gamification.dto.UserRankingDTO(ue.userAppId, SIZE(ue.badges)) " +
+                "new ch.heigvd.amt.gamification.model.CustomBadgesRanking(ue.userAppId, SIZE(ue.badges)) " +
             "FROM UserEntity AS ue " +
             "WHERE ue.app = :app " +
             "ORDER BY SIZE(ue.badges)")
-    List<UserRankingDTO> userRankingsByBadges(@Param("app") ApplicationEntity app);
+    List<BadgesRanking> userRankingsByBadges(@Param("app") ApplicationEntity app);
 }
