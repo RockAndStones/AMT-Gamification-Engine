aa54988e3ce878300b4698795c6a41cc27c9f74a
Naludrag <stephane.teixeiracarvalho@heig-vd.ch>
2020-12-16 10:35:35 +0100

    Refactor event processor

--- gamification-impl/src/main/java/ch/heigvd/amt/gamification/services/EventProcessor.java
+++ gamification-impl/src/main/java/ch/heigvd/amt/gamification/services/EventProcessor.java
@@ -32,29 +32,17 @@ public class EventProcessor {
                 // Verify that a badge is won
                 if (r.getBadge() != null) {
                     BadgeEntity badge = badgeRepository.findByNameAndAppApiKey(r.getBadge().getName(), app.getApiKey());
-                    if (!user.getBadges().contains(badge)) {
+                    if (!user.getBadges().contains(badge) && badge.getUsable()) {
                         user.getBadges().add(badgeRepository.findByNameAndAppApiKey(r.getBadge().getName(), app.getApiKey()));
                     }
                 }
-                // Add the points to the right pointScale
-                PointsUserEntity pointsUserEntity = pointsUserRepository.findPointsByUserIdAndPointScaleId(user.getId(), r.getPointScale().getId());
-                if(pointsUserEntity == null){
-                    pointsUserEntity = new PointsUserEntity();
-                    pointsUserEntity.setPoints(0.0);
-                    pointsUserEntity.setPointScale(r.getPointScale());
-                    pointsUserEntity.setUser(user);
-                    pointsUserRepository.save(pointsUserEntity);
-                }
-                PointsHistoryEntity pointsHistory = new PointsHistoryEntity();
-                pointsHistory.setEvent(event);
-                pointsHistory.setPointScale(r.getPointScale());
-                pointsHistory.setNbPoints(r.getPointsToAdd());
-                pointsHistoryRepository.save(pointsHistory);
 
-                double currentPoints = pointsUserEntity.getPoints() + r.getPointsToAdd();
-                pointsUserEntity.setPoints(currentPoints);
-                pointsUserRepository.save(pointsUserEntity);
-                // Add badges gained in the pointScale
+                // Add the points to the right pointScale
+                PointsUserEntity pointsUserEntity = addPointsToPointScale(user, r);
+                // To keep the history of points we add it to the repository
+                addPointsToHistory(event, r);
+
+                // Add badges gained in the pointScale a user can win multiple stage at once
                 for(StageEntity stage : stageRepository.findAllByPointScaleId(r.getPointScale().getId())){
                     if(stage.getPoints() <= pointsUserEntity.getPoints()){
                         if(!user.getBadges().contains(stage.getBadge()) && stage.getBadge().getUsable()) {
@@ -65,5 +53,28 @@ public class EventProcessor {
                 userRepository.save(user);
             }
         }
+    }
+
+    private void addPointsToHistory(EventEntity event, RuleEntity r) {
+        PointsHistoryEntity pointsHistory = new PointsHistoryEntity();
+        pointsHistory.setEvent(event);
+        pointsHistory.setPointScale(r.getPointScale());
+        pointsHistory.setNbPoints(r.getPointsToAdd());
+        pointsHistoryRepository.save(pointsHistory);
+    }
+
+    private PointsUserEntity addPointsToPointScale(UserEntity user, RuleEntity r) {
+        PointsUserEntity pointsUserEntity = pointsUserRepository.findPointsByUserIdAndPointScaleId(user.getId(), r.getPointScale().getId());
+        if(pointsUserEntity == null){
+            pointsUserEntity = new PointsUserEntity();
+            pointsUserEntity.setPoints(0.0);
+            pointsUserEntity.setPointScale(r.getPointScale());
+            pointsUserEntity.setUser(user);
+            pointsUserRepository.save(pointsUserEntity);
+        }
+        double currentPoints = pointsUserEntity.getPoints() + r.getPointsToAdd();
+        pointsUserEntity.setPoints(currentPoints);
+        pointsUserRepository.save(pointsUserEntity);
+        return pointsUserEntity;
     }
 }
